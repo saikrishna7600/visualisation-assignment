3a91822989f073945af9d7822316643a1e17e7b8
saikrishna7600 <70154749+saikrishna7600@users.noreply.github.com>
2023-11-10 15:15:39 +0000

    Update GDP.py

--- GDP.py
+++ GDP.py
@@ -1,12 +1,11 @@
-"""
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-#Create the Line plots
-def lineplot(gdp_df) :
-    years = list(gdp_df.index)
+#Create Line plots
+def lineplot(gdpc_df) :
+    years = list(gdpc_df.index)
     plt.xlim(years[0], years[-1])
-    plt.plot(gdp_df.index, gdp_df, label=gdp_df.columns)
+    plt.plot(gdpc_df.index, gdpc_df, label=gdpc_df.columns)
     plt.xlabel('Year')
     plt.ylabel('GDP')
     plt.title('GDP for India, UK, USA(1990 to 2018)')
@@ -15,31 +14,31 @@ def lineplot(gdp_df) :
     plt.show()
     
 #Create the PiePlot
-def piechart(gdp_df) :
-    gdp_1990 = gdp_df.loc[1990]
+def piechart(gdpc_df) :
+    gdp_1990 = gdpc_df.loc[1990]
     plt.pie(gdp_1990, autopct='% 1.1f %%', labels=gdp_1990.index)
     plt.title('GDP Distribiution b/w India, UK, USA in 1990')
     plt.savefig('gdp_dist_1990.png')
     plt.show()
 
-    gdp_2018 = gdp_df.loc[2018]
+    gdp_2018 = gdpc_df.loc[2018]
     plt.pie(gdp_2018, autopct='% 1.1f %%', labels=gdp_2018.index)
     plt.title('GDP Distribiution b/w India, UK, USA in 2018')
     plt.savefig('gdp_dist_2018.png')
     plt.show()
  
 #Create the Barplot
-def barchart(gdp_df) :
-    gdp_1990 = gdp_df.loc[1990]
-    gdp_2018 = gdp_df.loc[2018]
+def barchart(gdpc_df) :
+    gdp_1990 = gdpc_df.loc[1990]
+    gdp_2018 = gdpc_df.loc[2018]
     width = 0.25
     r = np.arange(3)
-    plt.bar(r, gdp_1990, color = 'b', 
-            width = width, edgecolor = 'black', 
+    plt.bar(r, gdp_1990, color = 'g', 
+            width = width, edgecolor = 'white', 
             label='1990')
 
-    plt.bar(r + width, gdp_2018, color = 'g', 
-            width = width, edgecolor = 'black', 
+    plt.bar(r + width, gdp_2018, color = 'r', 
+            width = width, edgecolor = 'white', 
             label='2018')
 
     plt.xticks(r + width/2,['India', 'UK', 'USA'])
@@ -50,15 +49,16 @@ def barchart(gdp_df) :
     plt.savefig('gdp_comparison.png')
     plt.show()
 
-#Read the data from csv file
+#Read the data of given dataset file
 df = pd.read_csv('GDP.csv', index_col='Country ')
 df.drop(['Country Code'], axis=1, inplace=True)
-gdp_df = df.loc[['India', 'United Kingdom', 'United States']].fillna(0)
-print(gdp_df)
-gdp_df.columns = list(map(int, gdp_df))
-gdp_df = gdp_df.transpose()[:-1]
-print(gdp_df)
 
-lineplot(gdp_df)
-piechart(gdp_df)
-barchart(gdp_df)
+gdpc_df = df.loc[['India', 'United Kingdom', 'United States']].fillna(0)
+print(gdpc_df)
+gdpc_df.columns = list(map(int, gdpc_df))
+gdpc_df = gdpc_df.transpose()[:-1]
+print(gdpc_df)
+
+lineplot(gdpc_df)
+piechart(gdpc_df)
+barchart(gdpc_df)
